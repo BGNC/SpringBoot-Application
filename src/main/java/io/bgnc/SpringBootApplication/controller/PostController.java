1400e8e00fcefbd15ecffe69f40f3d1a0f3de226
Bugra <71045060+TheGnc@users.noreply.github.com>
2022-03-26 00:34:41 +0300

    ResponseEntity PostController.java

--- src/main/java/io/bgnc/SpringBootApplication/controller/PostController.java
+++ src/main/java/io/bgnc/SpringBootApplication/controller/PostController.java
@@ -2,6 +2,8 @@ package io.bgnc.SpringBootApplication.controller;
 
 import io.bgnc.SpringBootApplication.dto.PostRequest;
 import io.bgnc.SpringBootApplication.dto.PostResponse;
+import io.bgnc.SpringBootApplication.exceptions.PostFoundNotFoundException;
+import io.bgnc.SpringBootApplication.exceptions.SubredditNotFoundException;
 import io.bgnc.SpringBootApplication.service.PostService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -22,26 +24,28 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping
-    public ResponseEntity createPost(@RequestBody PostRequest postRequest){
+    public ResponseEntity<Void> createPost(@RequestBody PostRequest postRequest) throws SubredditNotFoundException {
         postService.save(postRequest);
-        return new ResponseEntity(HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public PostResponse getPost(@PathVariable Long id){
+    public ResponseEntity<PostResponse> getPost(@PathVariable Long id) throws PostFoundNotFoundException {
 
-        return postService.getPost(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(postService.getPost(id));
 
     }
     @GetMapping("/")
-    public List<PostResponse> getAllPosts(){
+    public ResponseEntity<List<PostResponse>> getAllPosts(){
 
-        return postService.getAllPosts();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(postService.getAllPosts());
 
 
     }
     @GetMapping("/by-subreddit/{id}")
-    public List<PostResponse> getPostsBySubreddit(Long id){
+    public List<PostResponse> getPostsBySubreddit(Long id) throws SubredditNotFoundException {
 
         return postService.getPostsBySubreddit(id);
 
